e00e6f6d8026883a0b0e8b537a9f431a6315814e
Zouhir Chahoud <zouhir@zouhir.org>
2017-02-15 22:31:16 +1100

    copy webpack plugin bug + webpack linting fixes

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -22,137 +22,129 @@ module.exports = {
   },
 
   resolve: {
-		extensions: ['', '.jsx', '.js', '.json', '.scss'],
-		modulesDirectories: [
-			path.resolve(__dirname, "src/lib"),
-			path.resolve(__dirname, "node_modules"),
-			'node_modules'
-		],
-		alias: {
-			components: path.resolve(__dirname, "src/components"),    // used for tests
-			style: path.resolve(__dirname, "src/style"),
-      uploads: path.resolve(__dirname, "uploads"),
-			'react': 'preact-compat',
-			'react-dom': 'preact-compat'
-		}
-	},
-
-	module: {
-		preLoaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: path.resolve(__dirname, 'src'),
-				loader: 'source-map'
-			}
-		],
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'babel'
-			},
-			{
-				// Transform our own .(scss|css) files with PostCSS and CSS-modules
-				test: /\.(scss|css)$/,
-				include: [path.resolve(__dirname, 'src/components'), path.resolve(__dirname, 'src/containers')],
-				loader: ExtractTextPlugin.extract('style?singleton', [
-					`css-loader?modules&importLoaders=1&sourceMap=${CSS_MAPS}`,
-					'postcss-loader',
-					`sass-loader?sourceMap=${CSS_MAPS}`
-				].join('!'))
-			},
-			{
-				test: /\.(scss|css)$/,
-				exclude: [path.resolve(__dirname, 'src/components'), path.resolve(__dirname, 'src/containers')],
-				loader: ExtractTextPlugin.extract('style?singleton', [
-					`css?sourceMap=${CSS_MAPS}`,
-					`postcss`,
-					`sass?sourceMap=${CSS_MAPS}`
-				].join('!'))
-			},
-			{
-				test: /\.json$/,
-				loader: 'json'
-			},
-			{
-				test: /\.(xml|html|txt|md)$/,
-				loader: 'raw'
-			},
-			{
-				test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
-				loader: ENV==='production' ? 'file?name=[path][name]_[hash:base64:5].[ext]' : 'url'
-			}
-		]
-	},
-
-	postcss: () => [
-		autoprefixer({ browsers: 'last 2 versions' })
-	],
-
-	plugins: ([
-		new webpack.NoErrorsPlugin(),
-		new ExtractTextPlugin('style.css', {
-			allChunks: true,
-			disable: ENV!=='production'
-		}),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(ENV)
-		}),
-		new HtmlWebpackPlugin({
-			template: './index.html',
-			minify: { collapseWhitespace: true }
-		}),
-		new CopyWebpackPlugin([
-			{ from: './manifest.json', to: './' },
-			{ from: './favicon.ico', to: './' },
-			{ from: './_redirects', to: './' }
-		])
-	]).concat(ENV==='production' ? [
-		// strip out babel-helper invariant checks
-		new ReplacePlugin([{
-			// this is actually the property name https://github.com/kimhou/replace-bundle-webpack-plugin/issues/1
-			partten: /throw\s+(new\s+)?[a-zA-Z]+Error\s*\(/g,
-			replacement: () => 'return;('
-		}]),
-
-		new OfflinePlugin({
-			relativePaths: false,
-			AppCache: false,
+    extensions: ['', '.jsx', '.js', '.json', '.scss'],
+    modulesDirectories: [
+      path.resolve(__dirname, 'src/lib'),
+      path.resolve(__dirname, 'node_modules'),
+      'node_modules'
+    ],
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),    // used for tests
+      style: path.resolve(__dirname, 'src/style'),
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: path.resolve(__dirname, 'src'),
+        loader: 'source-map'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        // Transform our own .(scss|css) files with PostCSS and CSS-modules
+        test: /\.(scss|css)$/,
+        include: [path.resolve(__dirname, 'src/components'), path.resolve(__dirname, 'src/containers')],
+        loader: ExtractTextPlugin.extract('style?singleton', [
+          `css-loader?modules&importLoaders=1&sourceMap=${CSS_MAPS}`,
+          'postcss-loader',
+          `sass-loader?sourceMap=${CSS_MAPS}`
+        ].join('!'))
+      },
+      {
+        test: /\.(scss|css)$/,
+        exclude: [path.resolve(__dirname, 'src/components'), path.resolve(__dirname, 'src/containers')],
+        loader: ExtractTextPlugin.extract('style?singleton', [
+          `css?sourceMap=${CSS_MAPS}`,
+          `postcss`,
+          `sass?sourceMap=${CSS_MAPS}`
+        ].join('!'))
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.(xml|html|txt|md)$/,
+        loader: 'raw'
+      },
+      {
+        test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
+        loader: ENV === 'production' ? 'file?name=[path][name]_[hash:base64:5].[ext]' : 'url'
+      }
+    ]
+  },
+  postcss: () => [
+    autoprefixer({ browsers: 'last 2 versions' })
+  ],
+  plugins: ([
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('style.css', {
+      allChunks: true,
+      disable: ENV !== 'production'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(ENV)
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: { collapseWhitespace: true }
+    }),
+    new CopyWebpackPlugin([
+      { from: './manifest.json', to: './' },
+      { from: './favicon.ico', to: './' },
+      { from: './_redirects', to: './' },
+      { from: './_DATA', to: './_DATA' }
+    ])
+  ]).concat(ENV === 'production' ? [
+    // strip out babel-helper invariant checks
+    new ReplacePlugin([{
+      // this is actually the property name https://github.com/kimhou/replace-bundle-webpack-plugin/issues/1
+      partten: /throw\s+(new\s+)?[a-zA-Z]+Error\s*\(/g,
+      replacement: () => 'return;('
+    }]),
+    new OfflinePlugin({
+      relativePaths: false,
+      AppCache: false,
       excludes: ['_redirects'],
-			ServiceWorker: {
-				events: true
-			},
-			publicPath: '/'
-		})
-	] : []),
-
-	stats: { colors: true },
-
-	node: {
-		global: true,
-		process: false,
-		Buffer: false,
-		__filename: false,
-		__dirname: false,
-		setImmediate: false
-	},
-
-	devtool: ENV==='production' ? 'source-map' : 'cheap-module-eval-source-map',
-
-	devServer: {
-		port: process.env.PORT || 8080,
-		host: 'localhost',
-		colors: true,
-		publicPath: '/',
-		contentBase: './src',
-		historyApiFallback: true,
-		open: true,
-		proxy: {
-			// OPTIONAL: proxy configuration:
-			// '/optional-prefix/**': { // path pattern to rewrite
-			//   target: 'http://target-host.com',
-			//   pathRewrite: path => path.replace(/^\/[^\/]+\//, '')   // strip first path segment
-			// }
-		}
-	}
-};
+      ServiceWorker: {
+        events: true
+      },
+      publicPath: '/'
+    })
+  ] : []),
+  stats: { colors: true },
+  node: {
+    global: true,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false,
+    setImmediate: false
+  },
+  devtool: ENV === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
+  devServer: {
+    port: process.env.PORT || 8080,
+    host: 'localhost',
+    colors: true,
+    publicPath: '/',
+    contentBase: './src',
+    historyApiFallback: true,
+    open: true,
+    proxy: {
+      // OPTIONAL: proxy configuration:
+      // '/optional-prefix/**': { // path pattern to rewrite
+      //   target: 'http://target-host.com',
+      //   pathRewrite: path => path.replace(/^\/[^\/]+\//, '')   // strip first path segment
+      // }
+    }
+  }
+}
